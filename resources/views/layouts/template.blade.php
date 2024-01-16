22979b7b31cbb4d820b183b902aa7dfb3aad102e
unknown <ramata2447@gmail.com>
2024-01-16 13:20:55 +0700

    membuat template

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @yield('title')
+    <link rel="stylesheet" href="{{ asset('style/style.css') }}">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <div id="app">
+        @include('layouts.navbar')
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+</body>
+</html>
