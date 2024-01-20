4068fe28a4347a8ce266cd373ec0607eac29e12b
unknown <hafidaljabbaar@gmail.com>
2024-01-20 11:33:08 +0700

    navside v1 repair

--- resources/views/layouts/template_guru.blade.php
+++ resources/views/layouts/template_guru.blade.php
@@ -37,18 +37,37 @@
 </head>
 
 <body>
-    <div id="app">
-        <div class="container-scroll">
-            @include('layouts.sidebar')
-            <div class="container-fluid page-body-wrapper">
-                @include('layouts.navbar_v2')
-                <div class="main-panel">
+    <div class="container-scroller">
+        <!-- partial:partials/_sidebar.html -->
+        @include('layouts.sidebar')
+        <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
+          <!-- partial:partials/_navbar.html -->
+            @include('layouts.navbar_v2')
+          <!-- partial -->
+          <div class="main-panel">
+            <div class="content-wrapper">
+              <div class="row">
+                <div class="col-12 grid-margin stretch-card">
                     @yield('content')
                 </div>
+              </div>
+
             </div>
+            <!-- content-wrapper ends -->
+            <!-- partial:partials/_footer.html -->
+            <footer class="footer">
+              <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
+                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates</a> from Bootstrapdash.com</span>
+              </div>
+            </footer>
+            <!-- partial -->
+          </div>
+          <!-- main-panel ends -->
         </div>
-        
-    </div>
+        <!-- page-body-wrapper ends -->
+      </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
