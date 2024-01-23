ea53117380d17cdda20ba4e59b6843c55e872742
unknown <wulandarinurw@gmail.com>
2024-01-23 15:25:47 +0700

    Pengerjaan soal 50%

--- resources/views/layouts/template_guru.blade.php
+++ resources/views/layouts/template_guru.blade.php
@@ -66,9 +66,6 @@
         </div>
         <!-- page-body-wrapper ends -->
       </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
     <script src="{{ asset('js/custom.js') }}"></script>
 
     <!-- plugins:js -->
@@ -91,6 +88,8 @@
     <!-- Custom js for this page -->
     <script src="{{asset('assets')}}/js/dashboard.js"></script>
     <!-- End custom js for this page -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- resources/views/siswa/pengerjaan_quiz.blade.php
+++ resources/views/siswa/pengerjaan_quiz.blade.php
@@ -26,43 +26,68 @@
                                     <p class="card-description">Add class <code>.list-ticked</code> to
                                         <code>&lt;ul&gt;</code>
                                     </p>
+
                                 </div>
                             </div>
                         </div>
-                        <div class="col-6 grid margin">
+                        <div class="col-7 grid margin">
                             <div class="card" style="background:white;">
                                 <div class="card-body">
-                                    <h4 class="card-title">List with icon</h4>
-                                    <p class="card-description">Add class <code>.list-ticked</code> to
-                                        <code>&lt;ul&gt;</code>
-                                    </p>
-                                    <ul class="list-ticked">
-                                        <li>Lorem ipsum dolor sit amet</li>
-                                        <li>Consectetur adipiscing elit</li>
-                                        <li>Integer molestie lorem at massa</li>
-                                        <li>Facilisis in pretium nisl aliquet</li>
-                                        <li>Nulla volutpat aliquam velit&gt;</li>
-                                    </ul>
+                                    <form action="">
+
+                                        <p>Lorem ipsum dolor sit amet</p>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault"
+                                                id="flexRadioDefault1">
+                                            <label class="form-check-label" for="flexRadioDefault1">
+                                                A
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault"
+                                                id="flexRadioDefault2" checked>
+                                            <label class="form-check-label" for="flexRadioDefault2">
+                                                B
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault"
+                                                id="flexRadioDefault1">
+                                            <label class="form-check-label" for="flexRadioDefault1">
+                                                C
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault"
+                                                id="flexRadioDefault2" checked>
+                                            <label class="form-check-label" for="flexRadioDefault2">
+                                                D
+                                            </label>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-3 grid margin">
+                        <div class="col-3 grid margin mb-5">
                             <div class="card" style="background:white;">
+                                <div class="card-header">
+                                    Quiz Navigation
+                                </div>
                                 <div class="card-body">
-                                    <h4 class="card-title">List with icon</h4>
-                                    <p class="card-description">Add class <code>.list-ticked</code> to
-                                        <code>&lt;ul&gt;</code>
-                                    </p>
-                                    <ul class="list-ticked">
-                                        <li>Lorem ipsum dolor sit amet</li>
-                                        <li>Consectetur adipiscing elit</li>
-                                    </ul>
+                                    <div class="col-2">
+                                        <button>1</button>
+                                    </div>
                                 </div>
                             </div>
+                        </div>
+                        <div class="col-12 grid-margin mt-5"  style="background:white;">
+                            kkk<br>
+                            lll<br>
+                            lll
+                            ;;;;
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
